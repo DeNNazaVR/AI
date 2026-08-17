26c7af66a6d854a13734d758c7b6f100d95846ba
DeNNazaVR <denis.udp@gmail.com>
2026-08-17 22:16:28 +0300

    made local_neigborhood

--- local_neighborhood.py
+++ local_neighborhood.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 class Segment:
     l = 0
@@ -31,6 +32,8 @@ class Object:
     features = []
     def __init__(self, features1):
         self.features = features1
+    def __repr__(self):
+        return f"Object({self.features})"
 
 
 def point_to_pattern(p):
@@ -108,9 +111,48 @@ def is_more_general(p1, p2):
     return True
 
 
-        
+def gen_neighboors(x, feature_std, n_samples=1000,sigma=0.1,seed=14131232562):
+    rng = np.random.default_rng(seed)
+    noise = rng.normal(
+        loc=0,
+        scale=sigma * feature_std,
+        size=(n_samples, len(x.features))
+    )
+    x1 = np.array(x.features)
+    samples = x1 + noise
+    ans = []
+    for i in samples:
+        o = Object(i.tolist())
+        ans.append(o)
+    return ans
 
-# objc = [2, 3, 4]
+
+def get_dist(x, local_samples, feature_std):
+    dist = []
+    x1 = np.array(x.features)
+    for o in local_samples:
+        z = np.array(o.features)
+        dif = (z - x1) / feature_std
+        distance = np.sqrt(
+            np.sum(dif ** 2)
+        )
+        dist.append(distance)
+
+    return np.array(dist)
+
+
+def get_weights(dist, sigma):
+    ans = np.exp(
+        -(dist ** 2) / (sigma ** 2)
+    )
+    return ans
+
+objc = [2, 3, 4]
+o = Object(objc)
+feature_std = np.array([1, 2.4, 0.7])
+
+vals = gen_neighboors(o, feature_std)
+print(vals)
 # s1 = Segment(1, 6)
 # s2 = Segment(-1, 24)
 # s3 = Segment(4, 4)
